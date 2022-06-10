e1553db5e803ed2eb1f4351760ce975738848808
L2ncE <llance_24@foxmail.com>
2022-06-10 14:48:36 +0800

    feat(dao):新增gorm以及redis初始化

--- main.go
+++ main.go
@@ -1,7 +1,25 @@
 package main
 
-import "go-chess/config"
+import (
+	"fmt"
+	"go-chess/config"
+	mysql "go-chess/dao/gorm"
+	"go-chess/dao/redis"
+	"log"
+)
 
 func main() {
 	config.InitConfig()
+
+	if err := mysql.InitGormDB(); err != nil {
+		log.Printf("init gorm failed, err:%v\n", err)
+	} else {
+		log.Println("连接GORM成功!")
+	}
+
+	if err := redis.InitRedis(); err != nil {
+		fmt.Printf("init redis failed, err:%v\n", err)
+	} else {
+		log.Println("连接Redis成功!")
+	}
 }
